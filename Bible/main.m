fbcc91d5f5db5db853dddb6168fe528929ca763a
yons <123>
2014-05-27 23:15:06 +0800

    Initial Commit

---
+++ Bible/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bible
+//
+//  Created by yons on 14-5-27.
+//  Copyright (c) 2014年 pquanshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ChristAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ChristAppDelegate class]));
+    }
+}
